352ec2112a49d42f9f93f2e5eceed6a7276ceb6a
dungtran3864 <zungmr@gmail.com>
2022-07-02 18:27:45 -0400

    Move MongoDB connect to bin

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const indexRouter = require("./routes/index");
 
@@ -17,22 +16,5 @@ app.use(cookieParser());
 require('dotenv').config();
 
 app.use("/", indexRouter);
-
-const uri = process.env.ATLAS_URI;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
-client.connect((err) => {
-  console.log('MongoDB database connection established successfully');
-  client.close();
-});
-
-const port = process.env.PORT || 3100;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
 module.exports = app;
