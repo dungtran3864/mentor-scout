bdb5b7bf374957f2e8a72b1309877755c10bce3b
dungtran3864 <zungmr@gmail.com>
2022-08-07 13:50:24 -0400

    Add CSP

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const session = require('express-session');
-const passport = require('passport');
-const mongoose = require('mongoose');
+const session = require("express-session");
+const passport = require("passport");
+const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
 
 require("dotenv").config();
@@ -17,22 +17,31 @@ const app = express();
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to DB'));
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to DB"));
 
 app.use(logger("dev"));
 app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+  );
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
